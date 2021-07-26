89d80b7c1d23f1ce340f47393cb61c43b74c9aca
Matt Agius <mtagius@gmail.com>
2021-07-26 10:17:58 -0400

    updated scp syntax

--- get-files-from-pwnagotchi.bat
+++ get-files-from-pwnagotchi.bat
@@ -1,4 +1,4 @@
 ssh -o IdentitiesOnly=yes -i ./ssh/id_rsa pi@pwnagotchi.local "sudo cp -r /root/handshakes/ /home/pi/handshakes"
 mkdir handshakes\pcap
-scp -o IdentitiesOnly=yes -r -i ./ssh/id_rsa pi@pwnagotchi.local:/home/pi/handshakes/.  ./handshakes/pcap
+scp -o IdentitiesOnly=yes -r -i ./ssh/id_rsa pi@pwnagotchi.local:/home/pi/handshakes/*  ./handshakes/pcap
 ssh -o IdentitiesOnly=yes -i ./ssh/id_rsa pi@pwnagotchi.local "sudo rm -rf /home/pi/handshakes"
