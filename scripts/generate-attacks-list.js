c3903994c1b5feeac3975857fb46230b3286b840
your_username <ivy00johns@users.noreply.github.com>
2023-11-13 18:48:39 -0600

    - Tabs instead of spaces.

--- scripts/generate-attacks-list.js
+++ scripts/generate-attacks-list.js
@@ -12,142 +12,142 @@ function determineAttackType(wordlist, rule, mask) {
 
 	if ((wordlist.endsWith(".txt") || wordlist.endsWith(".dic")) && mask !== "") {
 		return 6; // Type --attack-mode=6 for .txt/.dic * mask
-    }
+	}
 
 	if (mask !== "") {
 		return 3; // Type --attack-mode=3 for masks
-    }
+	}
 
 	if (rule !== "" && wordlist === "" && mask === "") {
 		return 0; // Type --attack-mode=0 for .rule by itself
-    }
+	}
 
-    if (rule !== "" && (wordlist.endsWith(".txt") || wordlist.endsWith(".dic"))) {
+	if (rule !== "" && (wordlist.endsWith(".txt") || wordlist.endsWith(".dic"))) {
 		return 0; // Type --attack-mode=0 for .txt/.dic * .rule
-    }
+	}
 
-    return null; // Unknown combination
+	return null; // Unknown combination
 }
 
 // Function to get file paths from directories or handle single files
 function getFilePaths(directoryOrFile, allowedExtensions = []) {
-    if (fs.statSync(directoryOrFile).isDirectory()) {
-        const files = fs.readdirSync(directoryOrFile);
-        return files
-            .filter(file => allowedExtensions.length === 0 || allowedExtensions.some(ext => file.endsWith(ext)))
-            .map(file => path.join(directoryOrFile, file));
-    } else {
-        return [directoryOrFile];
-    }
+	if (fs.statSync(directoryOrFile).isDirectory()) {
+		const files = fs.readdirSync(directoryOrFile);
+		return files
+			.filter(file => allowedExtensions.length === 0 || allowedExtensions.some(ext => file.endsWith(ext)))
+			.map(file => path.join(directoryOrFile, file));
+	} else {
+		return [directoryOrFile];
+	}
 }
 
 // Function to generate attack combinations
 function generateAttacks() {
-    const attacks = [];
+	const attacks = [];
 
-    // Loop through wordlists
-    for (const wordlistDir of config.WORDLISTS) {
-        const wordlistFiles = getFilePaths(wordlistDir, [".txt"]);
+	// Loop through wordlists
+	for (const wordlistDir of config.WORDLISTS) {
+		const wordlistFiles = getFilePaths(wordlistDir, [".txt"]);
 
-        // Loop through dictionaries
-        for (const dictionaryDir of config.DICTIONARIES) {
-            const dictionaryFiles = getFilePaths(dictionaryDir, [".dic"]);
+		// Loop through dictionaries
+		for (const dictionaryDir of config.DICTIONARIES) {
+			const dictionaryFiles = getFilePaths(dictionaryDir, [".dic"]);
 
-            // Loop through rules
-            for (const ruleDir of config.RULES) {
-                const ruleFiles = getFilePaths(ruleDir, [".rule"]);
+			// Loop through rules
+			for (const ruleDir of config.RULES) {
+				const ruleFiles = getFilePaths(ruleDir, [".rule"]);
 
-                // Loop through masks
-                for (const mask of config.MASKS) {
-                    // Loop through wordlists
-                    for (const wordlist of wordlistFiles) {
-                        // Loop through rules
-                        for (const rule of ruleFiles) {
-                            const attackType = determineAttackType(wordlist, rule, mask);
+				// Loop through masks
+				for (const mask of config.MASKS) {
+					// Loop through wordlists
+					for (const wordlist of wordlistFiles) {
+						// Loop through rules
+						for (const rule of ruleFiles) {
+							const attackType = determineAttackType(wordlist, rule, mask);
 
-                            if (attackType !== null) {
-                                const attack = [`--attack-mode=${attackType}`];
+							if (attackType !== null) {
+								const attack = [`--attack-mode=${attackType}`];
 
-                                if (wordlist !== "") {
-                                    // Push the full file path
-                                    attack.push(wordlist);
-                                }
+								if (wordlist !== "") {
+									// Push the full file path
+									attack.push(wordlist);
+								}
 
-                                if (rule !== "") {
-                                    // Push the full file path
-                                    attack.push(rule);
-                                }
+								if (rule !== "") {
+									// Push the full file path
+									attack.push(rule);
+								}
 
-                                attacks.push(attack);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+								attacks.push(attack);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
-    // Add standalone --attack-mode=0 attacks for .rule files
-    for (const ruleDir of config.RULES) {
-        const ruleFiles = getFilePaths(ruleDir, [".rule"]);
-        for (const rule of ruleFiles) {
-            const attackType = determineAttackType("", rule, "");
-            if (attackType === 0) {
-                attacks.push([`--attack-mode=${attackType}`, "", rule]);
-            }
-        }
-    }
+	// Add standalone --attack-mode=0 attacks for .rule files
+	for (const ruleDir of config.RULES) {
+		const ruleFiles = getFilePaths(ruleDir, [".rule"]);
+		for (const rule of ruleFiles) {
+			const attackType = determineAttackType("", rule, "");
+			if (attackType === 0) {
+				attacks.push([`--attack-mode=${attackType}`, "", rule]);
+			}
+		}
+	}
 
-    // Add --attack-mode=3 attacks for mask alone
-    for (const mask of config.MASKS) {
-        const attackType = determineAttackType("", "", mask);
-        if (attackType === 3) {
-            attacks.push([`--attack-mode=${attackType}`, mask]);
-        }
-    }
+	// Add --attack-mode=3 attacks for mask alone
+	for (const mask of config.MASKS) {
+		const attackType = determineAttackType("", "", mask);
+		if (attackType === 3) {
+			attacks.push([`--attack-mode=${attackType}`, mask]);
+		}
+	}
 
-    // Add --attack-mode=6 attacks for (.txt * mask) and (.dic * mask)
-    for (const mask of config.MASKS) {
-        for (const wordlistDir of config.WORDLISTS) {
-            const wordlistFiles = getFilePaths(wordlistDir, [".txt"]);
-            for (const wordlist of wordlistFiles) {
-                const attackType = determineAttackType(wordlist, "", mask);
-                if (attackType === 6) {
-                    // Push the full file path
-                    attacks.push([`--attack-mode=${attackType}`, wordlist, mask]);
-                }
-            }
-        }
+	// Add --attack-mode=6 attacks for (.txt * mask) and (.dic * mask)
+	for (const mask of config.MASKS) {
+		for (const wordlistDir of config.WORDLISTS) {
+			const wordlistFiles = getFilePaths(wordlistDir, [".txt"]);
+			for (const wordlist of wordlistFiles) {
+				const attackType = determineAttackType(wordlist, "", mask);
+				if (attackType === 6) {
+					// Push the full file path
+					attacks.push([`--attack-mode=${attackType}`, wordlist, mask]);
+				}
+			}
+		}
 
-        for (const dictionaryDir of config.DICTIONARIES) {
-            const dictionaryFiles = getFilePaths(dictionaryDir, [".dic"]);
-            for (const dictionary of dictionaryFiles) {
-                const attackType = determineAttackType(dictionary, "", mask);
-                if (attackType === 6) {
-                    // Push the full file path
-                    attacks.push([`--attack-mode=${attackType}`, dictionary, mask]);
-                }
-            }
-        }
-    }
+		for (const dictionaryDir of config.DICTIONARIES) {
+			const dictionaryFiles = getFilePaths(dictionaryDir, [".dic"]);
+			for (const dictionary of dictionaryFiles) {
+				const attackType = determineAttackType(dictionary, "", mask);
+				if (attackType === 6) {
+					// Push the full file path
+					attacks.push([`--attack-mode=${attackType}`, dictionary, mask]);
+				}
+			}
+		}
+	}
 
-    return attacks;
+	return attacks;
 }
 
 // Function to write attacks to a file
 function writeAttacksToFile(attacks, filePath) {
-    const content = `const attacks = ${JSON.stringify(attacks, null, 2)};\n\nmodule.exports = attacks;`;
+	const content = `const attacks = ${JSON.stringify(attacks, null, 2)};\n\nmodule.exports = attacks;`;
 
-    fs.writeFileSync(filePath, content, "utf8");
+	fs.writeFileSync(filePath, content, "utf8");
 }
 
 // Main function
 function main() {
-    const attacks = generateAttacks();
+	const attacks = generateAttacks();
 
-    writeAttacksToFile(attacks, config.HASHCAT_ATTACK_LISTS);
+	writeAttacksToFile(attacks, config.HASHCAT_ATTACK_LISTS);
 
-    console.log(`Generated attacks saved to ${config.HASHCAT_ATTACK_LISTS}.`);
+	console.log(`Generated attacks saved to ${config.HASHCAT_ATTACK_LISTS}.`);
 }
 
 main();
--- scripts/generate-hashcat-scripts.js
+++ scripts/generate-hashcat-scripts.js
@@ -1,72 +1,71 @@
 const fs = require("fs");
 const path = require("path");
 const config = require("../config");
-const attacks = require(`.${config.HASHCAT_ATTACK_LISTS}`);
 
 function generateHashcatCommands(attacks) {
-    const hcCapxFiles = fs.readdirSync(config.LOCAL_HCCAPX_DIRECTORY).filter(file => file.endsWith(".hc22000"));
+	const hcCapxFiles = fs.readdirSync(config.LOCAL_HCCAPX_DIRECTORY).filter(file => file.endsWith(".hc22000"));
 
-    for (const hcCapxFile of hcCapxFiles) {
-        const hcCapxFilePath = path.join(config.LOCAL_HCCAPX_DIRECTORY, hcCapxFile);
-        const sessionName = `${path.basename(hcCapxFile, path.extname(hcCapxFile))}_${getRandomInt()}`;
-        const scriptFilePath = path.join(config.HASHCAT_ATTACK_SCRIPTS, `${path.basename(hcCapxFile, path.extname(hcCapxFile))}.txt`);
+	for (const hcCapxFile of hcCapxFiles) {
+		const hcCapxFilePath = path.join(config.LOCAL_HCCAPX_DIRECTORY, hcCapxFile);
+		const sessionName = `${path.basename(hcCapxFile, path.extname(hcCapxFile))}_${getRandomInt()}`;
+		const scriptFilePath = path.join(config.HASHCAT_ATTACK_SCRIPTS, `${path.basename(hcCapxFile, path.extname(hcCapxFile))}.txt`);
 
-        // Create an array to store script lines
-        const scriptLines = [];
+		// Create an array to store script lines
+		const scriptLines = [];
 
-        for (const attack of attacks) {
-            const attackType = attack[0];
-            switch (attackType) {
-                case "--attack-mode=0":
-                    scriptLines.push(generateType0Command(attack, sessionName, hcCapxFilePath));
-                    break;
-                case "--attack-mode=3":
-                    scriptLines.push(generateType3Command(attack, sessionName, hcCapxFilePath));
-                    break;
-                case "--attack-mode=6":
-                    scriptLines.push(generateType6Command(attack, sessionName, hcCapxFilePath));
-                    break;
-                // Add more cases for other attack types if needed
-                default:
-                    console.error(`Unknown attack type: ${attackType}`);
-            }
-        }
+		for (const attack of attacks) {
+			const attackType = attack[0];
+			switch (attackType) {
+				case "--attack-mode=0":
+					scriptLines.push(generateType0Command(attack, sessionName, hcCapxFilePath));
+					break;
+				case "--attack-mode=3":
+					scriptLines.push(generateType3Command(attack, sessionName, hcCapxFilePath));
+					break;
+				case "--attack-mode=6":
+					scriptLines.push(generateType6Command(attack, sessionName, hcCapxFilePath));
+					break;
+				// Add more cases for other attack types if needed
+				default:
+					console.error(`Unknown attack type: ${attackType}`);
+			}
+		}
 
-        // Write the script lines to the script file
-        fs.writeFileSync(scriptFilePath, scriptLines.join("\n"), "utf8");
-    }
+		// Write the script lines to the script file
+		fs.writeFileSync(scriptFilePath, scriptLines.join("\n"), "utf8");
+	}
 }
 
 function generateType0Command(attack, sessionName, hcCapxFilePath) {
-    const attackType = attack[0];
-    const wordlist = attack[1];
-    const rule = attack[2];
+	const attackType = attack[0];
+	const wordlist = attack[1];
+	const rule = attack[2];
 
-    if (wordlist != "") {
-        return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" --rules-file="${rule}" -S "${wordlist}"`;
-    } else {
-        return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" --rules-file="${rule}"`;
-    }
+	if (wordlist != "") {
+		return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" --rules-file="${rule}" -S "${wordlist}"`;
+	} else {
+		return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" --rules-file="${rule}"`;
+	}
 }
 
 function generateType3Command(attack, sessionName, hcCapxFilePath) {
-    const attackType = attack[0];
-    const mask = attack[1];
+	const attackType = attack[0];
+	const mask = attack[1];
 
-    return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" "${mask}"`;
+	return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" "${mask}"`;
 }
 
 function generateType6Command(attack, sessionName, hcCapxFilePath) {
-    const attackType = attack[0];
-    const wordlist = attack[1];
-    const mask = attack[2];
+	const attackType = attack[0];
+	const wordlist = attack[1];
+	const mask = attack[2];
 
-    return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" "${wordlist}" "${mask}"`;
+	return `hashcat --hash-type=${config.HASH_TYPE} ${attackType} --session ${sessionName} --hwmon-temp-abort=${config.ABORT_TEMPERATURE} -w ${config.ABORT_WAIT_TIME} --potfile-path "${config.LOCAL_POTFILES_DIRECTORY}/${sessionName}-potfile.txt" --outfile="${config.LOCAL_OUTPUT_FILE_DIRECTORY}/${sessionName}-outfile.txt" "${hcCapxFilePath}" "${wordlist}" "${mask}"`;
 }
 
 // Function to generate a random 4-digit integer
 function getRandomInt() {
-    return Math.floor(1000 + Math.random() * 9000);
+	return Math.floor(1000 + Math.random() * 9000);
 }
 
-generateHashcatCommands(attacks);
+generateHashcatCommands(config.HASHCAT_ATTACK_LISTS);
