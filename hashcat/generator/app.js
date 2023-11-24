fc121356577b0ee38557c48113acfb6403d28616
your_username <ivy00johns@users.noreply.github.com>
2023-11-23 18:44:09 -0600

    - Progress.

--- hashcat/generator/app.js
+++ hashcat/generator/app.js
@@ -1,32 +1,51 @@
 const fs = require("fs");
 const config = require("./config");
 
-function generateCombinations(words) {
-	const combinations = [];
+// Function to generate permutations of a given array of words
+function generatePermutations(words, maxWordsUsed) {
+	const permutations = [];
 
-	// Generate combinations
-	for (let i = 0; i < words.length; i++) {
-		for (let j = i + 1; j < words.length; j++) {
-			const combined = words[i] + words[j];
-			combinations.push(combined);
+	// Helper function to swap elements in an array
+	function swap(arr, i, j) {
+		const temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 
-			// Add reversed combination as well
-			const reversedCombined = words[j] + words[i];
-			combinations.push(reversedCombined);
+	// Helper function to generate permutations using backtracking
+	function generate(index, k) {
+		if (index === k - 1) {
+			// Add the current permutation to the result
+			permutations.push(words.slice(0, k).join(''));
+			return;
+		}
+
+		for (let i = index; i < words.length; i++) {
+			// Swap elements to create a new permutation
+			swap(words, index, i);
+			// Recursively generate permutations for the remaining elements
+			generate(index + 1, k);
+			// Swap back to backtrack
+			swap(words, index, i);
 		}
 	}
 
-	return combinations;
+	// Generate permutations for different values of k up to maxWordsUsed
+	for (let k = 1; k <= Math.min(maxWordsUsed, words.length); k++) {
+		generate(0, k);
+	}
+
+	return permutations;
 }
 
 // Example usage with the provided list
 const initialWords = config.wordList;
-const result = generateCombinations(initialWords);
+const result = generatePermutations(initialWords, config.maxWordsUsed);
 
-// Print count of combos generated in the terminal
-console.log(`Generated ${result.length} combinations.`);
+// Print count of unique permutations generated in the terminal
+console.log(`Generated ${result.length} unique permutations.`);
 
-// Sort combinations alphabetically
+// Sort permutations alphabetically
 const sortedResult = result.sort();
 
 // Print specified number of items in the terminal
@@ -38,7 +57,7 @@ console.log(sortedResult.slice(0, itemsToPrint));
 const permutationsToGenerate = config.generatePermutations;
 const outputFile = config.exportFileName;
 
-// Sort combinations alphabetically before writing to the file
-const sortedCombinations = result.sort();
-fs.writeFileSync(outputFile, sortedCombinations.slice(0, permutationsToGenerate).join("\n"));
-console.log(`Combinations written to ${outputFile}`);
+// Sort permutations alphabetically before writing to the file
+const sortedPermutations = result.sort();
+fs.writeFileSync(outputFile, sortedPermutations.slice(0, permutationsToGenerate).join("\n"));
+console.log(`Permutations written to ${outputFile}`);
